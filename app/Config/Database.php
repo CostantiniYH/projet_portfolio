757c0c24cb51203877e2bffc6188970d93503970
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-03 00:55:49 +0100

    déploiement...

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -16,10 +16,10 @@ use PDOException;
 
 class Database {
     public static function connect() {
-        $host = 'localhost';
-        $db = 'db_portfolio';
-        $user = 'root';
-        $pass = '';
+        $host = $_ENV['DB_HOST'];
+        $db   = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASS'];
         $charset = 'utf8mb4';
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
         $options = [
