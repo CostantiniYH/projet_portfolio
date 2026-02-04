906fe1d006ee634e39c6b92f6072766cd3224973
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-04 18:20:33 +0100

    nouvelle branche principale pour le local test

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -21,7 +21,7 @@ class Database {
         $user = $_ENV['DB_USER'];
         $pass = $_ENV['DB_PASS'];
         $charset = 'utf8mb4';
-        $dsn = "pgsql:host=$host;dbname=$db;charset=$charset";
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
