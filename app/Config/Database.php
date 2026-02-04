69489420124bfa58322620ae193e4179fad973da
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-04 19:17:16 +0100

    comm

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -20,7 +20,7 @@ class Database {
         $db   = $_ENV['DB_NAME'];
         $user = $_ENV['DB_USER'];
         $pass = $_ENV['DB_PASS'];
-        $dsn = "pgsql:host=$host;dbname=$db";
+        $dsn = "pgsql:host=$host;dbname=$db"; // Avec PostgreSql le dsn ne prend pas charset en paramètres
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
