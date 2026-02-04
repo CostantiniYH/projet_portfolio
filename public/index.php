4730451cb2d17767e9a094856ce1a9bddf4ae1cf
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-04 20:01:43 +0100

    remise à la normal

--- public/index.php
+++ public/index.php
@@ -4,20 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Test temporaire
-echo "DEBUT INDEX<br>";
-var_dump(__DIR__);
-echo "<br>";
-
-if (file_exists(__DIR__ . '/../app/Views/partials/navbar.php')) {
-    echo "Le fichier navbar existe !<br>";
-    require __DIR__ . '/../app/Views/partials/navbar.php';
-} else {
-    echo "Le fichier navbar N'EXISTE PAS !<br>";
-    echo "Chemin cherché ta mère : " . __DIR__ . '/../app/Views/partials/navbar.php<br>';
-}
-
-// Ton code normal ensuite...
 session_start();
 require dirname(__DIR__) . '/vendor/autoload.php';
 
