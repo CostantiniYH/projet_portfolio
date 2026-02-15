88fb96fabedfea667c403140ce885b6b6507494f
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-15 20:52:29 +0100

    navbar, presentation

---
+++ app/Views/missions/activite_ecole.php
@@ -0,0 +1,49 @@
+<section class="row mb-5 bg-sec p-5">
+    <h1 class="text-center">Liste des activités</h1>
+</section>
+<main class="container">
+    <section class="row gap-4 mx-auto">
+        <div class="card mx-auto p-1 p-1 hvr-overline-from-left" style="width: 18rem;">
+            <img src="<?= BASE_URL ?>uploads/activites/chat_last_version.png" alt="" 
+            class="card-img-top img-fluid" style="height: 13rem;">
+            <div class="card-body">
+                <h5 class="card-title">Activité 1 - Chat instantané</h5>
+                <p class="card-text text-justifier">Développement d'un chat instanatané en PHP et MySql</p>
+                <a href="#" class="btn btn-activite stretched-link">Cliquer pour voir le détail</a>
+            </div>
+        </div>
+
+        <div class="card mx-auto p-1 hvr-overline-from-left" style="width: 18rem;">
+            <img src="<?= BASE_URL ?>uploads/activites/ubuntu_install.png" alt="" 
+            class="card-img-top img-fluid" style="height: 13rem;">
+            <div class="card-body">
+                <h5 class="card-title">Activité 2 - Ubuntu server</h5>
+                <p class="card-text">Mise en place d'une infrastructure complète pour héberger des applications 
+                    sur Ubuntu server</p>
+                <a href="#" class="btn btn-activite stretched-link">Cliquer pour voir le détail</a>
+            </div>
+        </div>
+        
+        <div class="card mx-auto p-1 hvr-overline-from-left" style="width: 18rem;">
+            <img src="<?= BASE_URL ?>uploads/activites/gd_classe.jpg" alt="" 
+            class="card-img-top img-fluid" style="height: 13rem;">
+            <div class="card-body">
+                <h5 class="card-title">Activité 3 - Gestion des classes</h5>
+                <p class="card-text">Créer une BDD MySQL sous Wamp server pour simuler une gestion des classes
+                    avec contraintes. L'application et 
+                </p>
+                <a href="#" class="btn btn-activite stretched-link">Cliquer pour voir le détail</a>
+            </div>
+        </div>
+
+        <div class="card mx-auto p-1 hvr-overline-from-left" style="width: 18rem;">
+            <img src="<?= BASE_URL ?>uploads/activites/chat_last_version.png" alt="" 
+            class="card-img-top img-fluid" style="height: 13rem;">
+            <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+                <a href="#" class="btn btn-activite stretched-link">Cliquer pour voir le détail</a>
+            </div>
+        </div>
+    </section>
+</main>
--- app/Views/missions/index.m.php
+++ app/Views/missions/index.m.php
@@ -19,14 +19,14 @@
     </section>
 
     <section class="row gap-4 mb-5">
-        <div class="col-md card p-1 border-0 bg- shadow position-relative">
+        <div class="col-md card p-0 border-0 shadow position-relative">
             <img class="img-filtre " src="<?= BASE_URL ?>uploads/activites/ecole.png" alt="">
             <div class="position-absolute text-white top-50 start-50 translate-middle">
                 <h4 class="">Activité école</h4>
             </div>
             <a href="activites-ecole" class="stretched-link"></a>
         </div>
-        <div class="col-md card border-0 p-1 bg- shadow position-relative">
+        <div class="col-md card border-0 p-0 shadow position-relative">
             <img class="img-filtre w-100" src="<?= BASE_URL ?>uploads/activites/entreprise.png" alt="">
             <div class="position-absolute text-white top-50 start-50 translate-middle">
                 <h4 class="">Activité Entreprise</h4>
@@ -36,7 +36,7 @@
     </section>
 
     <section class="row">
-        <div class="img-synthese border-0 p-1 bg- shadow position-relative">
+        <div class="img-synthese border-0 p-0 shadow position-relative">
             <img class="img-filtre w-100" src="<?= BASE_URL ?>uploads/activites/table_synthese.png" 
             alt="" style="max-height: 8rem;">            
             <div class="position-absolute text-white top-50 start-50 translate-middle">
