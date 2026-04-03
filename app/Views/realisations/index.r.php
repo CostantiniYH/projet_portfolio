c40762b78b4634b8b48061404e3abd972f31077f
CostantiniYH <costantiniyaacov@gmail.com>
2026-04-03 18:46:07 +0200

    E5 : rédaction activité gestio utilisateurs...

--- app/Views/realisations/index.r.php
+++ app/Views/realisations/index.r.php
@@ -82,7 +82,8 @@
                     <h5 class="card-title">Activité 2 - Gestion des utilisateurs et des groupes locaux dans un réseau poste-à-poste</h5>
                     <p class="card-text"></p>
                 </div>
-                <a href="#<?= BASE_URL ?>" class="mt-auto btn btn-activite stretched-link card-footer w-100">Cliquer pour voir le détail</a>
+                <a href="<?= BASE_URL ?>resspurces/masques/RA-gestion_users.pdf" class="mt-auto btn btn-activite stretched-link card-footer w-100"
+                target="_blank">Cliquer pour voir le détail</a>
             </div>   
         </div>
         
@@ -96,8 +97,8 @@
                     <p class="card-text">
                     </p>
                 </div>
-                <a href="#<?= BASE_URL ?>ressources/masques/.pdf" 
-                class="mt-auto btn btn-activite stretched-link card-footer w-100" target="_blank">Cliquer pour voir le détail</a>
+                <a href="#<?= BASE_URL ?>ressources/masques/.pdf" class="mt-auto btn btn-activite stretched-link card-footer w-100" 
+                target="_blank">Cliquer pour voir le détail</a>
             </div>
         </div>
 
